a67d35a6a74f335def6e2d05ee3a14ae2593f038
elizabethboterf <boterfelizabeth@gmail.com>
2021-08-30 15:12:50 -0500

    refresh github pages

---
+++ build/precache-manifest.1f1c2a5c87673c2bab367778c9fd753f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17b4ab9000419f84ef4fc3ffe6c98abc",
+    "url": "/pomodoro-timer/index.html"
+  },
+  {
+    "revision": "92945632b6db733bde7b",
+    "url": "/pomodoro-timer/static/css/main.cb9e985a.chunk.css"
+  },
+  {
+    "revision": "e679157d0c95d7009c04",
+    "url": "/pomodoro-timer/static/js/2.ebcb8928.chunk.js"
+  },
+  {
+    "revision": "a9dc7dd75f8156044ebb12f4929856ca",
+    "url": "/pomodoro-timer/static/js/2.ebcb8928.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "92945632b6db733bde7b",
+    "url": "/pomodoro-timer/static/js/main.2f2dc771.chunk.js"
+  },
+  {
+    "revision": "5b934f73dc3f50a7b525",
+    "url": "/pomodoro-timer/static/js/runtime-main.cb3a558a.js"
+  }
+]);
